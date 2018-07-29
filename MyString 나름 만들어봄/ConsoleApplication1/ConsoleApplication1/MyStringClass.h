15e1c4f2514e7a242d487a6a99a932d2c9466a70
Buchen-Apple <41614050+Buchen-Apple@users.noreply.github.com>
2018-07-29 23:05:18 +0900

    2018-07-29 최초 업데이트

---
+++ MyString 나름 만들어봄/ConsoleApplication1/ConsoleApplication1/MyStringClass.h
@@ -0,0 +1,73 @@
+#ifndef __MY_STRING_CLASS_H__
+#define __MY_STRING_CLASS_H__
+
+
+#include <iostream>
+
+using namespace std;
+
+class MyString
+{
+	char* m_String;
+
+	// 널문자 제외 길이
+	size_t m_len;
+
+public:
+
+	// ---------------
+	// 생성자 파트
+	// ---------------
+	// 보이드 생성자
+	MyString()
+		:m_len(0), m_String(nullptr)
+	{}
+
+	// 값 입력받는 생성자
+	MyString(const char* str);
+
+	// 복사 생성자 (깊은 복사)
+	MyString(const MyString& copy);
+
+	// ---------------
+	// 연산자 오버로딩 파트
+	// ---------------
+	// 대입 연산자 (깊은 복사)
+	MyString& operator=(const MyString& copy);
+
+	// + 연산자 오버로딩.
+	// A+B 문자열의 결과를 객체로 반환
+	MyString operator+(const MyString& str);
+
+	// += 연산자 오버로딩
+	// A 문자열에 B를 붙인다.
+	MyString& operator+=(const MyString& ref);
+
+	// == 연산자 오버로딩
+	// A와 B 비교
+	// 같으면 true, 다르면 false 반환
+	bool operator==(const MyString& ref);
+
+	// << 연산자 오버로딩
+	// 화면 출력용 
+	friend ostream& operator<<(ostream& os, const MyString& ref);
+
+	// >> 연산자 오버로딩
+	// 입력받기 용 
+	friend istream& operator<<(istream& os, const MyString& ref);
+
+
+	~MyString();
+};
+
+
+// << 연산자 오버로딩
+// 화면 출력용 
+ostream& operator<<(ostream& os, const MyString& ref);
+
+// >> 연산자 오버로딩
+// 입력받기 용 
+istream& operator<<(istream& is, const MyString& ref);
+
+
+#endif // !__MY_STRING_CLASS_H__
---
+++ _C# 가지고놀기/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    // 동물들 베이스
+    class MammalBase
+    {
+        // 0 : 개 / 1 : 고양이
+        int iType;
+
+        public MammalBase(int iType)
+        {
+            this.iType = iType;
+        }
+
+        public virtual void howl(int Input)
+        { }
+    }
+
+    class Dog : MammalBase
+    {
+        public Dog(int type) : base(type)
+        {}
+
+        public override void howl(int Input)
+        {
+            int randVal;
+
+            unsafe
+            {
+                Random rand = new Random();
+                randVal = rand.Next(0, 1574) + (int)&randVal;
+
+                randVal = (randVal * (Input + 1)) % 3;               
+            }
+
+            // 유저 액션과 완전히 동일하면 기분 좋음.
+            // 다르면, 상황에 따라 다른 반응
+            if (Input == 0)
+            {
+                switch (randVal)
+                {
+                    case 0:
+                        Console.Write("개 : 기분 매우매우매우 좋다멍!");
+                        break;
+                    case 1:
+                        Console.Write("개 : 좋다멍!");
+                        break;
+                    case 2:
+                        Console.Write("개 : 완전 별로다멍...");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            else if (Input == 1)
+            {
+                switch (randVal)
+                {
+                    case 0:
+                        Console.Write("개 : 완전 별로다멍...");
+                        break;
+                    case 1:
+                        Console.Write("개 : 기분 매우매우매우 좋다멍!");
+                        break;
+                    case 2:
+                        Console.Write("개 : 좋다멍!");                       
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+
+            else
+            {
+                switch (randVal)
+                {
+                    case 0:
+                        Console.Write("개 : 완전 별로다멍...");
+                        break;
+                    case 1:
+                        Console.Write("개 : 좋다멍!");
+                        break;
+                    case 2:                       
+                        Console.Write("개 : 기분 매우매우매우 좋다멍!");
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+           
+        }
+    }
+
+    class Cat : MammalBase
+    {
+        public Cat(int type) : base(type)
+        { }
+
+        public override void howl(int Input)
+        {
+            int randVal;
+
+            unsafe
+            {
+                Random rand = new Random();
+                randVal = rand.Next(0, 12575) + (int)&randVal;
+
+                randVal = (randVal * (Input + 1)) % 3;
+            }
+
+            if (Input == 0)
+            {
+                switch (randVal)
+                {
+                    case 0:
+                        Console.Write("고양이 : 기분 매우매우매우 좋다냥!");
+                        break;
+                    case 1:
+                        Console.Write("고양이 : 좋다냥!");
+                        break;
+                    case 2:
+                        Console.Write("고양이 : 완전 별루다냥...!");
+                        break;
+                }
+            }
+
+            else if (Input == 1)
+            {
+                switch (randVal)
+                {
+                    case 0:
+                        Console.Write("고양이 : 완전 별루다냥...!");
+                        break;
+                    case 1:
+                        Console.Write("고양이 : 기분 매우매우매우 좋다냥!");
+                        break;
+                    case 2:
+                        Console.Write("고양이 : 좋다냥!");                       
+                        break;
+                }
+            }
+
+            else
+            {
+                switch (randVal)
+                {
+                    case 0:
+                        Console.Write("고양이 : 완전 별루다냥...!");                        
+                        break;
+                    case 1:
+                        Console.Write("고양이 : 좋다냥!");
+                        break;
+                    case 2:
+                        Console.Write("고양이 : 기분 매우매우매우 좋다냥!");
+                        break;
+                }
+            }
+        }
+    }
+
+    class User
+    {
+        // 유저의 행동. 누군가 씻기기
+        public void Wash(ref MammalBase Nobody, int Input)
+        {
+            Console.Write("동물의 반응 >>> [");
+            Nobody.howl(Input);
+            Console.WriteLine("]");
+        }
+    }
+
+
+    class MAIN
+    {
+        static void Main(string[] args)
+        {
+            // 개, 고양이 생성
+            MammalBase Dog = new Dog(0);
+            MammalBase Cat = new Cat(0);
+
+            // 유저 생성
+            User Player = new User();
+
+            while (true)
+            {
+                // 액션 선택
+                Console.Write("동물을 관리합니다 (1 : 털 손질 / 2 : 목욕 / 3 : 밥주기) : ");
+                string str = Console.ReadLine();
+                int Input = int.Parse(str);
+
+                Input--;
+
+                // 개 씻기기
+                Player.Wash(ref Dog, Input);
+
+                // 고양이 씻기기
+                Player.Wash(ref Cat, Input);
+
+                Console.WriteLine();             
+            }
+
+        }
+    }
+}
