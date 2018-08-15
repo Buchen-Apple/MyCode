25867af3525877a45433fbc3a5593c3565f0afc7
Bucheon-Apple <lij2026@naver.com>
2018-08-15 19:42:38 +0900

    2018-08-15 락프리큐 2차완

--- 9_실무용 게임서버 프로그래밍/10차과제_락프리 큐 만들기/LFQ_Main/LFQ_Main/LockFree_Queue.h
+++ 9_실무용 게임서버 프로그래밍/10차과제_락프리 큐 만들기/LFQ_Main/LFQ_Main/LockFree_Queue.h
@@ -75,8 +75,8 @@ namespace Library_Jingyu
 	template <typename T>
 	CLF_Queue<T>::CLF_Queue(bool bPlacementNew)
 	{
-		// 내부 노드 셋팅. 최초는 0개
-		m_NodeCount = 0;
+		// 내부 노드 셋팅. 최초는 1개 (Interlock으로 감소시키기 때문에 0이되면 아무것도 없는것으로 체크하기 위함.)
+		m_NodeCount;
 
 		// 메모리풀 생성자 호출
 		m_MPool = new CMemoryPool<st_LFQ_NODE>(0, bPlacementNew);
@@ -88,10 +88,9 @@ namespace Library_Jingyu
 		// 만든 더미는 헤더와 테일이 가리킨다.
 		// 이 때 만들어진 더미는, 데이터가 없는 더미이다.
 		m_stpHead.m_pPoint = m_MPool->Alloc();
-		m_stpTail.m_pPoint = m_stpHead.m_pPoint;
+		m_stpHead.m_pPoint->m_stpNextBlock = nullptr;
 
-		// 테일의 Next는 null이다.			
-		m_stpTail.m_pPoint->m_stpNextBlock = nullptr;
+		m_stpTail.m_pPoint = m_stpHead.m_pPoint;
 	}
 
 	// 소멸자
@@ -141,20 +140,17 @@ namespace Library_Jingyu
 			pLocalNext = LocalTail.m_pPoint->m_stpNextBlock;
 
 			// 정말 m_stpTail이 LocalTail와 같다면 로직 진행
-			if (LocalTail.m_pPoint == m_stpTail.m_pPoint &&
+			if (LocalTail.m_pPoint == m_stpTail.m_pPoint && 
 				LocalTail.m_l64Count == m_stpTail.m_l64Count)
 			{
-
-				// 스냅샷으로 찍은 next가 null일때만 로직 진행.
+				// Next가 Null이면 로직 진행
 				if (pLocalNext == nullptr)
 				{
-					// 인큐 작업(노드와 노드 연결 작업)
-					if (InterlockedCompareExchangePointer((PVOID*)&LocalTail.m_pPoint->m_stpNextBlock, NewNode, pLocalNext) == pLocalNext)
+					if (InterlockedCompareExchangePointer((PVOID*)&m_stpTail.m_pPoint->m_stpNextBlock, NewNode, pLocalNext) == pLocalNext)
 					{
 						// 성공했으면 이제 Tail 이동작업
-						// 얘는 그냥 시도한다. 누가 바꿨으면 실패하고 끝.
+						// 얘는 그냥 시도한다. 누가 바꿨으면 실패하고 끝.			
 						InterlockedCompareExchange128((LONG64*)&m_stpTail, LocalTail.m_l64Count + 1, (LONG64)NewNode, (LONG64*)&LocalTail);
-
 						break;
 					}
 				}
@@ -164,9 +160,9 @@ namespace Library_Jingyu
 					// null이 아니라면 Tail이동작업 시도
 					InterlockedCompareExchange128((LONG64*)&m_stpTail, LocalTail.m_l64Count + 1, (LONG64)pLocalNext, (LONG64*)&LocalTail);
 				}
-			}
-		}
-
+			}		
+			
+		}		
 
 		// 큐 내부의 사이즈 1 증가
 		InterlockedIncrement(&m_NodeCount);
@@ -180,64 +176,68 @@ namespace Library_Jingyu
 	template <typename T>
 	int CLF_Queue<T>::Dequeue(T& OutData)
 	{
+		// 큐 내부의 사이즈 1 감소
 		// 노드가 없으면 -1 리턴
-		if (m_NodeCount == 0)
+		if (InterlockedDecrement(&m_NodeCount) < 0)
 			return -1;
 
 		// 락프리 구조 --------------------------
 		// 디큐는, 기본적으로 현재 헤드가 가리키고 있는 노드의, 다음 노드의 값을 리턴한다.
 		// 헤드는 무조건 더미를 가리키는 것이다.
-		alignas(16) st_NODE_POINT LocalHead;
-		alignas(16) st_NODE_POINT TempLocalHead;
-		alignas(16) st_NODE_POINT LocalTail;
-		st_LFQ_NODE* pLocalNext;
+		alignas(16) st_NODE_POINT LocalHead, TempLocalHead;
+		st_LFQ_NODE *pLocalNext, *pDeleteHead, *pLocalTail;
 
 		while (true)
 		{
-			// head 스냅샷
-			TempLocalHead.m_pPoint = LocalHead.m_pPoint = m_stpHead.m_pPoint;
-			TempLocalHead.m_l64Count = LocalHead.m_l64Count = m_stpHead.m_l64Count;			
-
-			// tail 스냅샷
-			LocalTail.m_pPoint = m_stpTail.m_pPoint;
-			LocalTail.m_l64Count = m_stpTail.m_l64Count;
+			// head, 이동시도용 tail, Delete노드 스냅샷
+			pDeleteHead = TempLocalHead.m_pPoint = LocalHead.m_pPoint = m_stpHead.m_pPoint;
+			LocalHead.m_l64Count = m_stpHead.m_l64Count;
+			TempLocalHead.m_l64Count = m_stpTail.m_l64Count;
 
 			// head->Next 스냅샷
-			pLocalNext = LocalHead.m_pPoint->m_stpNextBlock;
+			pLocalNext = pDeleteHead->m_stpNextBlock;
 
-			// 헤더가 같을 경우
-			if (LocalHead.m_pPoint == m_stpHead.m_pPoint &&
-				LocalHead.m_pPoint->m_stpNextBlock == m_stpHead.m_pPoint->m_stpNextBlock)
+			// tail 스냅샷
+			pLocalTail = m_stpTail.m_pPoint;			
+
+			// 정말 m_stpHead이 LocalHead와 같다면 로직 진행
+			if (m_stpHead.m_pPoint == LocalHead.m_pPoint && 
+				m_stpHead.m_l64Count == LocalHead.m_l64Count)
 			{
-				// 헤더와 테일이 같은지 체크
-				if (LocalHead.m_pPoint == LocalTail.m_pPoint &&
-					LocalHead.m_pPoint->m_stpNextBlock == LocalTail.m_pPoint->m_stpNextBlock &&
-					pLocalNext == nullptr)
+				// 헤더와 테일이 같고, pLocalNext가 null인지 체크
+				if (pDeleteHead == pLocalTail)
 				{
-					m_CDump->Crash();
-					//return - 1;
+					if (pLocalNext == nullptr)
+					{
+						m_CDump->Crash();
+						//return - 1;
+					}
+
+					else
+					{
+						// tail 이동 시도			
+						InterlockedCompareExchange128((LONG64*)&m_stpTail, LocalHead.m_l64Count + 1, (LONG64)pLocalNext, (LONG64*)&TempLocalHead);
+					}
 				}
 
 				// 헤더와 테일이 다르면 디큐작업 진행
-				else if (InterlockedCompareExchange128((LONG64*)&m_stpHead, LocalHead.m_l64Count + 1, (LONG64)pLocalNext, (LONG64*)&LocalHead))
+				else
 				{
-					// tail 이동 시도
-					LocalHead.m_pPoint = TempLocalHead.m_pPoint;
-					LocalHead.m_l64Count = m_stpTail.m_l64Count;
-					InterlockedCompareExchange128((LONG64*)&m_stpTail, LocalHead.m_l64Count + 1, (LONG64)pLocalNext, (LONG64*)&LocalHead);
-
 					// out 매개변수에 데이터 셋팅
-					OutData = TempLocalHead.m_pPoint->m_Data;
+					OutData = pLocalNext->m_Data;	
 
-					// 이동 전, 헤더를 메모리풀에 반환
-					m_MPool->Free(TempLocalHead.m_pPoint);
-					break;
+					// 헤더 이동 시도
+					if (InterlockedCompareExchange128((LONG64*)&m_stpHead, LocalHead.m_l64Count + 1, (LONG64)pLocalNext, (LONG64*)&LocalHead))
+					{						
+						// 이동 전의 헤더를 메모리풀에 반환
+						m_MPool->Free(pDeleteHead);
+						break;
+					}
 				}
 			}
-		}
 
-		// 큐 내부의 사이즈 1 감소
-		InterlockedDecrement(&m_NodeCount);
+			
+		}
 
 		// 성공했으니 0 리턴
 		return 0;
